04c31707340384391fa11d55b76394517d65d916
Gary McKellar <gmckellar@mavenlink.com>
2018-11-02 17:47:45 -0600

    Chapter 2 - Multiplying does not mutate amount

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -3,9 +3,17 @@ require "dollar"
 describe Dollar do
   let(:five) { described_class.new 5 }
 
-  it "can multiply" do
-    five.times 2
+  describe "#times" do
+    let(:multiplier) { 2 }
+    subject { five.times multiplier }
 
-    expect(five.amount).to eq 10
+    it "multiplies" do
+      expect(subject.amount).to eq 10
+      expect(five.times(5).amount).to eq 25
+    end
+
+    it "does not mutate amount" do
+      expect { subject }.to_not change five, :amount
+    end
   end
 end
